09d2326863b5df7beffeb9c370427be971251e18
khoiw04 <khoi.w04@gmail.com>
2026-06-07 19:40:20 +0700

    Add deferred

---
+++ fourth-element/deferred/src/main.rs
@@ -0,0 +1,21 @@
+use gpui::{
+    AppContext, Application, Context, IntoElement, ParentElement, Render, Window, WindowOptions,
+    deferred, div,
+};
+
+struct RootView;
+
+impl Render for RootView {
+    fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
+        div().child(deferred(div()).with_priority(10))
+    }
+}
+
+fn main() {
+    Application::new().run(|app| {
+        app.open_window(WindowOptions::default(), |_window, app| {
+            app.new(|_cx| RootView)
+        })
+        .unwrap();
+    });
+}
